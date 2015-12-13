9c282653ba6c650bf49d612455e3d09566f73903
vtange <victortang1992@gmail.com>
2015-12-13 07:57:11 +0000

    clearer comment

--- app.js
+++ app.js
@@ -9,17 +9,21 @@ var mongoose = require('mongoose');
 console.log("APP.JS")
 console.log("start app")
 var app = express();
-
+//--------------------
 // view engine setup
 console.log("set view engine")
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+//--------------------
+// basic setup
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+//--------------------
+// server routes
 console.log("loading routes")
 var routes = require('./routes/index');
 app.use('/', routes);
